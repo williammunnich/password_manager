1d36c93d0340e783c8fdda14eefdab8799107238
williammunnich <williammunnich@gmail.com>
2018-10-11 13:44:22 -0500

    added a .gitignore XD

--- src/com/password_manager/main.java
+++ src/com/password_manager/main.java
@@ -1,15 +1,18 @@
 package com.password_manager;
 
+import java.io.FileNotFoundException;
 import java.lang.*;
 
 import java.text.SimpleDateFormat;
 
 public class main{
    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
-   public static void main(String[] args) {
+   public static void main(String[] args) throws FileNotFoundException {
        password.write_to_txt_timestamp();
        password.write_to_txt_WUP();
         System.out.println(decryptor.decrypted("Ћ΄ΖЋ΄Ζ"));
         System.out.println(searcher.searcherFound("password.txt","########" ));
         System.out.println("done");
-}}
+}
+
+}
