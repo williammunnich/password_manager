d782a927c7c9eb6357846d819e8f4262c741651a
David <dhoc1901@mpsedu.org>
2018-10-30 12:41:43 -0500

    Pseudo Code 1
    Pseudo Code on main and entry files.

--- src/com/password_manager/entry.java
+++ src/com/password_manager/entry.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 public class entry {
     public static void entries() {
-        StringBuffer masterKey = password.MasterPassword();
-        String passcode = password.write_to_txt_WUP();
-        StringBuffer passEncrypted = encryptor.encrypted(passcode);
+        StringBuffer masterKey = password.MasterPassword();                                             //Creates the master account and stores the encrypted version of your master password as a variable in this file
+        String passcode = password.write_to_txt_WUP();                                                  //Runs for a specific website and stores that password as a variable
+        StringBuffer passEncrypted = encryptor.encrypted(passcode);                                     //Encrypts the password and stores that encryption
 
         System.out.println("Do you wish to decrypt your password? Answer Y or N.");
         Scanner userInput = new Scanner(System.in);
-        String response = userInput.nextLine();
-            while (response != "Y") {
+        String response = userInput.nextLine();                                                         //Stores user response for whether or not to encrypt
+            while (response != "Y") {                                                                   //Checks to see if your response is Y and if it isn't Y or N it prompts you to try again
 
                 if (response.equals("Y")) {
                     System.out.println("Enter your Master Password");
-                    StringBuffer PassComp = encryptor.encrypted(userInput.nextLine());
-                    String PassComp1 = PassComp.toString();
-                    String masterKey1 = masterKey.toString();
+                    StringBuffer PassComp = encryptor.encrypted(userInput.nextLine());                  //Creates an encrypted version of what is entered when prompting for your master password
+                    String PassComp1 = PassComp.toString();                                             //Turns the encrypted version from StringBuffer to String for comparison
+                    String masterKey1 = masterKey.toString();                                           //Same thing as last line but for a different variable
 
                     if (PassComp1.equals(masterKey1)) {
                         System.out.println("Decrypted Password: " + decryptor.decrypted(passEncrypted));
-                        break;
+                        break;                                                                          //Stops the code from continuing to run
                     } else {
                         System.out.println("Wrong Password");
 
@@ -32,7 +32,7 @@ public class entry {
                 } else {
                     System.out.println("Not a valid response");
                     System.out.println("Try again, make sure it is Y or N:");
-                    response = userInput.nextLine();
+                    response = userInput.nextLine();                                                    //Resets the value of the user response
                 }
             }
         }
--- src/com/password_manager/main.java
+++ src/com/password_manager/main.java
@@ -9,7 +9,7 @@ public class main{
     private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
 
     public static void main(String[] args) {
-        password.write_to_txt_timestamp();
-        entry.entries();
+        password.write_to_txt_timestamp(); //Creates a timestamp pn th text file
+        entry.entries(); //Runs everything in use
         }
     }
